2f804240dbebcf0631b4080d5f065a06889fa780
Aleksandr Kochkurkin <info@debitop.com>
2022-06-06 23:25:49 +0300

    5.2.2. Реализовать Store<T extends Base> [#157]

---
+++ src/test/java/ru/job4j/generic/RoleStoreTest.java
@@ -0,0 +1,71 @@
+package ru.job4j.generic;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class RoleStoreTest {
+
+    @Test
+    public void whenAddAndFindThenRoleNameIsPetr() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        Role result = store.findById("1");
+        assertThat(result.getRoleName(), is("Petr"));
+    }
+
+    @Test
+    public void whenAddAndFindThenRoleIsNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        Role result = store.findById("10");
+        assertNull(result);
+    }
+
+    @Test
+    public void whenAddDuplicateAndFindRoleNameIsPetr() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.add(new Role("1", "Maxim"));
+        Role result = store.findById("1");
+        assertThat(result.getRoleName(), is("Petr"));
+    }
+
+    @Test
+    public void whenReplaceThenRoleNameIsMaxim() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.replace("1", new Role("1", "Maxim"));
+        Role result = store.findById("1");
+        assertThat(result.getRoleName(), is("Maxim"));
+    }
+
+    @Test
+    public void whenNoReplaceRoleThenNoChangeRoleName() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.replace("10", new Role("10", "Maxim"));
+        Role result = store.findById("1");
+        assertThat(result.getRoleName(), is("Petr"));
+    }
+
+    @Test
+    public void whenDeleteRoleThenRoleIsNull() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.delete("1");
+        Role result = store.findById("1");
+        assertNull(result);
+    }
+
+    @Test
+    public void whenNoDeleteRoleThenRoleNameIsPetr() {
+        RoleStore store = new RoleStore();
+        store.add(new Role("1", "Petr"));
+        store.delete("10");
+        Role result = store.findById("1");
+        assertThat(result.getRoleName(), is("Petr"));
+    }
+}
