6d65a0055c2389538f08e1261d8c06be9d03197d
Aleksandr Kochkurkin <info@debitop.com>
2022-06-16 09:14:56 +0300

    5. Очередь на двух стеках [#160 #311762]

--- src/main/java/ru/job4j/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
@@ -7,6 +9,10 @@ public class SimpleQueue<T> {
     private int countOut = 0;
 
     public T poll() {
+        if (countIn == 0 && countOut == 0) {
+            throw new NoSuchElementException();
+        }
+
         if (countOut == 0) {
             while (countIn != 0) {
                 out.push(in.pop());
