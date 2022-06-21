45c0c86fb258fcab69806231a555a79e2e4d160b
Aleksandr Kochkurkin <info@debitop.com>
2022-06-21 23:52:31 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,23 +1,27 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", children=" + children +
-                ", birthday=" + birthday +
-                '}';
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return children == user.children && name.equals(user.name) && birthday.equals(user.birthday);
+//    }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
@@ -26,11 +30,13 @@ public class User {
     }
 
     public static void main(String[] args) {
-        User user1 = new User("Alex", 2, new GregorianCalendar(2001, 02, 10));
-        User user2 = new User("Alex", 2, new GregorianCalendar(2001, 02, 10));
+        Calendar calendar = Calendar.getInstance();
+        User user1 = new User("Alex", 2, calendar);
+        User user2 = new User("Alex", 2, calendar);
         Map<User, Object> map = new HashMap<>();
         map.put(user1, new Object());
         map.put(user2, new Object());
         map.forEach((key, value) -> System.out.println(key + " : " + value));
+        map.forEach((key, value) -> System.out.println(key.hashCode() + " : " + value));
     }
 }
