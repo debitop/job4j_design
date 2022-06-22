2c7760ef3cc2e287f45ede02a251af6879b1cb20
Aleksandr Kochkurkin <info@debitop.com>
2022-06-22 23:05:35 +0300

    3. Переопределить только hashCode [#1003]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -3,11 +3,17 @@ package ru.job4j.map;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
     private int children;
     private Calendar birthday;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
 
     public User(String name, int children, Calendar birthday) {
         this.name = name;
