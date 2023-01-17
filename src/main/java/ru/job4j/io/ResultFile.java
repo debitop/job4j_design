b4ca6b8d500f1e821a029e191b714ead3cd27d13
Aleksandr Kochkurkin <info@debitop.com>
2023-01-17 22:22:24 +0300

    0.1. FileOutputStream. [#252488]

---
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9; j++) {
+                    String k = ((i + 1) * (j + 1)) + "";
+                    if (k.length() == 1) {
+                        k = k + " ";
+                    }
+                    out.write((k + " ").getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+            out.write(System.lineSeparator().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
