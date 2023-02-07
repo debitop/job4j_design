ec8ff0ce0912ca0dde4c838680ead874fafec87f
Aleksandr Kochkurkin <info@debitop.com>
2023-02-07 23:53:20 +0300

    7. Анализ доступности сервера [#859 #388128]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -5,19 +5,17 @@ import java.io.*;
 public class Analysis {
     public void unavailable(String source, String target) {
         try (BufferedReader read = new BufferedReader(new FileReader(source));
-             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(target)))) {
+             PrintWriter out = new PrintWriter(target)) {
             boolean broken = false;
-            String s = "";
             for (String line = read.readLine(); line != null; line = read.readLine()) {
                 if (!broken && (line.startsWith("400") || line.startsWith("500"))) {
-                    s = line.substring(line.indexOf(" ")) + ";";
+                    out.append(line.substring(line.indexOf(" "))).append(";");
                     broken = true;
                 }
                 if (broken && !(line.startsWith("400") || line.startsWith("500"))) {
-                    s = s + line.substring(line.indexOf(" ") + 1);
                     broken = false;
-                    System.out.println(s);
-                    out.println(s);
+                    out.append(line.substring(line.indexOf(" ") + 1)).append(" ")
+                            .append(System.lineSeparator());
                 }
             }
         } catch (IOException e) {
